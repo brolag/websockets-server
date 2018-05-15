f62805412ad3de9ef5c8246be55004841fc613d9
Alfredo Bonilla <brolag@live.com>
2018-05-14 21:43:24 -0600

    Remove unnecessary comments

--- server.js
+++ server.js
@@ -1,19 +1,13 @@
-// 1.  Very simple websocket server:
-// 	1a. Echo incoming message to all connected clients
-// 	1b. Generate random numbers and emit to connected clients
 var ws = require('nodejs-websocket');
 
-// 2. Server for incoming/outgoing chats
 var server = ws.createServer(function (connection) {
 	console.log('New Chat connection established.', new Date().toLocaleTimeString());
 
 	connection.on('text', function (msg) {
-		// simple object transformation (= add current time)
 		var msgObj = JSON.parse(msg);
 		msgObj.newDate = new Date().toLocaleTimeString();
 		var newMsg = JSON.stringify(msgObj);
 
-		// echo message including the new field to all connected clients
 		server.connections.forEach(function (connection) {
 			connection.sendText(newMsg);
 		});
@@ -24,7 +18,6 @@ var server = ws.createServer(function (connection) {
 	});
 
 	connection.on('error', function (err) {
-		// only throw if something else happens than Connection Reset
 		if (err.code !== 'ECONNRESET') {
 			console.log('Error in Chat Socket connection', err);
 			throw err;
